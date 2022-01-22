c18e9c83c1291bbefc96f399efc869dd0cf9c0d7
xin zhang <xin.zhang@optus.com.au>
2022-01-22 22:57:55 +1100

    add build

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "ts-loader"],
+        use: ["ts-loader"],
       },
       {
         test: /\.css$/,
